ea04e7bb69677c9bc7624006b8880d3468bbacde
siphesihle02 <spherhmlungwana@gmail.com>
2021-11-17 19:57:37 +0000

    Added a Db context class

---
+++ DeveloperTest Fruit SA/Database/Db.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using DeveloperTest_Fruit_SA.Models;
+
+namespace DeveloperTest_Fruit_SA.Database
+{
+    public class Db :DbContext
+    {
+        public Db() : base("productsDatabase")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
