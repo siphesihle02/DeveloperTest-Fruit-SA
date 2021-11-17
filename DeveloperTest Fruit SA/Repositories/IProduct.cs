9dcb4cde6f5ebd456867962040a50f065cad36d8
siphesihle02 <spherhmlungwana@gmail.com>
2021-11-17 22:18:11 +0000

    Added a product Interface

---
+++ DeveloperTest Fruit SA/Repositories/IProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeveloperTest_Fruit_SA.Models;
+
+namespace DeveloperTest_Fruit_SA.Repositories
+{
+   public interface IProduct : IDisposable
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int productId); 
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
