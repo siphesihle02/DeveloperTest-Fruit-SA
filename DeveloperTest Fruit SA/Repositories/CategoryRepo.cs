106e89f08c577a6141f6f626d0c8fb3d9f54e296
siphesihle02 <spherhmlungwana@gmail.com>
2021-11-18 02:24:01 +0000

    Added a category interface

---
+++ DeveloperTest Fruit SA/Repositories/CategoryRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeveloperTest_Fruit_SA.Repositories
+{
+    public class CategoryRepo
+    {
+    }
+}
---
+++ DeveloperTest Fruit SA/Repositories/ICatetogory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeveloperTest_Fruit_SA.Models;
+
+namespace DeveloperTest_Fruit_SA.Repositories
+{
+   public interface ICatetogory :IDisposable
+    {
+        IEnumerable<Category> GetCategory();
+        Category GeCategoryByID(int categoryId);
+        void InsertCategory(Category category);
+        void DeleteCategory(int categoryId);
+        void UpdateCategory(Category category);
+        void Save();
+    }
+}
+
+    
+
